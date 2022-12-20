cff4fa4202c91215f6cfdb107986acbcd5e390a6
Leo <conceicao.leonardo9@gmail.com>
2022-12-20 12:36:52 -0300

    adicionando oque foi alterado

--- src/teste/Exec.java
+++ src/teste/Exec.java
@@ -2,7 +2,7 @@ package teste;
 
 public class Exec {
     public static void main(String[] args) {
-        String texto = "teste pro git";
+        String texto = "agr eu mudei o texto";
 
         System.out.println(texto);
     }
